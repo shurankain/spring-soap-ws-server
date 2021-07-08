6a7e23f24347df5928e70af806fa47808e52accf
shurankain <shurankain@gmail.com>
2021-07-08 11:36:51 +0200

    main class renamed

--- src/main/java/com/shurankain/soaptest/springsoaptest/SpringSoapWsServerApp.java
+++ src/main/java/com/shurankain/soaptest/springsoaptest/SpringSoapWsServerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSoapWsServer {
+public class SpringSoapWsServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSoapWsServer.class, args);
+        SpringApplication.run(SpringSoapWsServerApp.class, args);
     }
 
 }
